b6b7a760f415f600dd34a7b17231b46234373434
Flávio Dutra <flaviomdutra@outlook.com>
2023-06-04 12:35:29 -0300

    initial commit

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { createStore } from "redux";
+
+import rootReducer from "./modules/rootReducer";
+import { ICartState } from "./modules/cart/types";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+export interface IState {
+  cart: ICartState;
+}
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+export default store;
